3fe0f8dfe4a222865fd3a217883bfd4bca58b9d0
Azizbek2396 <aziznurmuhammadov1996@gmail.com>
2021-08-29 23:18:43 +0500

    some changes added

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express')
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
+const flash = require('connect-flash')
 const app = express()
+
+let sessionOptions = session({
+    secret: "JavaScript is so cool!",
+    store: MongoStore.create({client: require('./db')}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }
+})
+
+app.use(sessionOptions)
+app.use(flash())
+
+app.use(function(req, res, next) {
+    res.locals.user = req.session.user
+    next()
+})
 
 const router = require('./router')
 
--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
-const usersCollection = require('../db').collection("users")
+const bcrypt = require("bcryptjs")
+const usersCollection = require('../db').db().collection("users")
 const validator = require('validator')
+const md5 = require('md5')
 
-let User = function(data) {
+let User = function(data, getAvatar) {
     this.data = data
     this.errors = []
+    if(getAvatar == undefined) {getAvatar = false}
+    if(getAvatar) { this.getAvatar() }
 }
 
 User.prototype.cleanUp = function() {
@@ -19,38 +23,72 @@ User.prototype.cleanUp = function() {
     }
 }
 
-User.prototype.validate = function() {
-    if(this.data.username == "") { this.errors.push("You must provide a username.") }
-    if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Usernam can only contain letters and numbers") }
-    if(!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
-    if(this.data.password == "") { this.errors.push("You must provide a password.") }
-    if(this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push('Password must be at least 12 characters.')}
-    if(this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
-    if(this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push('Username must be at least 3 characters.')}
-    if(this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
+User.prototype.validate = function () {
+    return new Promise(async (resolve, reject) => {
+        if(this.data.username == "") { this.errors.push("You must provide a username.") }
+        if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Usernam can only contain letters and numbers") }
+        if(!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
+        if(this.data.password == "") { this.errors.push("You must provide a password.") }
+        if(this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push('Password must be at least 12 characters.')}
+        if(this.data.password.length > 50) {this.errors.push("Password cannot exceed 50 characters.")}
+        if(this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push('Username must be at least 3 characters.')}
+        if(this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
+    
+        // Only if username is valid then check to see if its already taken
+        if(this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+            let usernameExists = await usersCollection.findOne({username: this.data.username})
+            if (usernameExists) {this.errors.push("That username is already taken.")}
+        }
+    
+        // Only if email is valid then check to see if its already taken
+        if( validator.isEmail(this.data.email)) {
+            let emailExists = await usersCollection.findOne({email: this.data.email})
+            if (emailExists) {this.errors.push("That email is already taken.")}
+        }
+        resolve()
+    })
 }
 
-User.prototype.login = function(collback) {
-    this.cleanUp()
-    usersCollection.findOne({username: this.data.username}, (err, attemptedUser) => {
-        if(attemptedUser && attemptedUser.password == this.data.password) {
-            collback('Congrats!!!!')
-        }else {
-            collback("Invalid usernam / password")
-        }
+User.prototype.login = function() {
+    return new Promise((resolve, reject) => {
+        this.cleanUp()
+        usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
+            if(attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+                this.data = attemptedUser
+                this.getAvatar()
+                resolve('Congrats!!!!')
+            }else {
+                reject("Invalid usernam / password")
+            }
+        }).catch(function(){
+            reject("Please try again later.")
+        })
     })
 }
 
 User.prototype.register = function() {
-    // Step #1: Validate user data
-    this.cleanUp()
-    this.validate()
+    return new Promise(async (resolve, reject) => {
+        // Step #1: Validate user data
+        this.cleanUp()
+        await this.validate()
+    
+        //Step #2: Only if there are no validation errors then save the user data into a database
+    
+        if(!this.errors.length) { 
+            // hash user password
+            let salt = bcrypt.genSaltSync(10)
+            this.data.password = bcrypt.hashSync(this.data.password, salt)
+            await usersCollection.insertOne(this.data)
+            this.getAvatar()
+            resolve()
+        } else {
+            reject(this.errors)
+        }
+    })
+}
 
-    //Step #2: 
-
-    if(!this.errors.length) {
-        usersCollection.insertOne(this.data)
-    }
+User.prototype.getAvatar = function() {
+    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 
